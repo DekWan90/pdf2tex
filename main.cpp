e15be132e7c245b0761e1d4402760456b3407e61
Izwan Azmi <izwanazmi90@gmail.com>
2021-07-30 16:29:17 +0800

    Testing with pdftoppm

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    std::string tajuk = "./sample/input.pdf";
+
+    system( "rm -rfv pdftoppm" );
+    system( "mkdir pdftoppm" );
+    system( ( "pdftoppm -jpeg -r 300 " + tajuk + " pdftoppm/output" ).c_str() );
+
     return 0;
 }
